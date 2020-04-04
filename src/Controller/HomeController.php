2d88e96ba4441c5184ac5a4a3611e32b0437765f
amir shadanfar <a.shadanfar.it@gmail.com>
2020-04-04 15:01:52 +0300

    make style to the main page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,10 +17,18 @@ class HomeController extends AbstractController
     {
         $developers = $this->getDoctrine()->getRepository(Developer::class)->findAll();
         $tasks = $this->getDoctrine()->getRepository(Task::class)->findAll();
+        if (!$developers || !$tasks)
+            throw new \Exception('You should run console command to continue!');
+
+        $estimated_time = StrategyA::getMinimumDuration($developers, $tasks);
 
         return $this->render('home/index.html.twig', [
             'developers'       => $developers,
-            'minimum_duration' => StrategyA::getMinimumDuration($developers, $tasks),
+            'minimum_duration' => [
+                'weeks' => round($estimated_time, 2),
+                'days'  => round($estimated_time * 7, 2),
+                'hours' => round($estimated_time * 7 * 24, 2),
+            ],
         ]);
     }
 }
